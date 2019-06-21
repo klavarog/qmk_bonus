95ddf49e8fc35269ce2ba1340bf324647313272d
bouncepaw <bouncepaw2@ya.ru>
2019-06-21 18:19:13 +0500

    Fix alcorithm
    To implement: Russian kcs, overrides

--- chordgen.rb
+++ chordgen.rb
@@ -1,11 +1,16 @@
+require 'pp'
+
 should_count_chords = false
 config_path = "./chords.ini"
 
-syms = {}
+$syms = {}
 
-('a'..'z').each { |ch| syms[ch] = "KC_#{ch.upcase}" }
-('0'..'9').each { |ch| syms[ch] = "KC_#{ch}" }
+('a'..'z').each { |ch| $syms[ch] = "KC_#{ch.upcase}" }
+('0'..'9').each { |ch| $syms[ch] = "KC_#{ch}" }
 
+def kc(k)
+  $syms[k]
+end
 
 should_ignore_next_elem_for_config_path = false
 ARGV.each do |arg|
@@ -43,31 +48,29 @@ def brace_expr(title, block)
 EOK
 end
 
-# TODO: implement kc
-def kc(k); k; end
-
 class Chord
   attr_reader :combo_event, :combo_array, :combo_key, :case_expr
+
   def initialize(left_hand, right_hand)
-    result_is_kc = left_hand[0] == '(' and left_hand[1] == ')'
+    result_is_kc = not(left_hand[0] == '(' and left_hand[-1] == ')')
 
     result    = result_is_kc ?
                   "tap_code16(#{kc left_hand});\n" :
                   left_hand[1..-2]
-    result_id = result_is_kc ? result : result.hash.to_s
+    result_id = result.hash.abs.to_s
 
     combo_event_id = "combo_event_#{result_id}"
     combo_array_id = "combo_array_#{result_id}"
-    keys = right_hand.split.map { |k| kc k }
+    keys = right_hand.chars.map { |k| kc k }
 
-    @combo_event = "  #{combo_event_id},\n"
+    @combo_event = "#{combo_event_id}, //#{result.chomp}\n  "
     @combo_array = brace_expr("const uint16_t PROGMEM #{combo_array_id}[] =",
-                              "#{keys.join(',')}, COMBO_END") + ";\n"
-    @combo_key   = "[#{combo_event_id}] = COMBO_ACTION(#{combo_array_id}),\n"
+                              "#{keys.join(', ')}, COMBO_END") + ";\n"
+    @combo_key   = "[#{combo_event_id}] = COMBO_ACTION(#{combo_array_id}),\n  "
     @case_expr = <<-EOK
-    case #{@combo_event_id}:
+    case #{combo_event_id}:
       if (pressed) {
-        #{@result}
+        #{result}
       }
       break;
 EOK
@@ -76,6 +79,7 @@ end
 
 class Layer
   attr_reader :combo_events, :combo_arrays, :combo_keys, :combo_switch
+
   def initialize(name, chords)
     @combo_events = chords.map(&:combo_event).join
     @combo_arrays = chords.map(&:combo_array).join
@@ -87,7 +91,7 @@ class Layer
   end
 end
 
-end
+
 class ChordedKeeb
   def initialize(layers)
     @combo_events_enum =
@@ -102,14 +106,14 @@ class ChordedKeeb
 
     @process_combo_event =
       brace_expr('void process_combo_event(uint8_t combo_index, bool pressed)',
-                 layers.map(&:combo_switch).join)
+                 layers.map(&:combo_switch).join('else '))
   end
 
   def as_string
-    @combo_events_enum
-    + @combo_arrays_declaration
-    + @combo_keys_array
-    + @process_combo_event
+    @combo_events_enum +
+      @combo_arrays_declaration +
+      @combo_keys_array +
+      @process_combo_event
   end
 end
 
@@ -118,7 +122,9 @@ read_result = []
 curr_section = {:header => nil, :contents => []}
 
 File.open(config_path) do |f|
-  while f.gets
+  while true
+    f.gets
+    read_result << curr_section and break if $_ == nil # On last line
     next if $_.empty_line?
 
     if $_.ini_header?
@@ -131,10 +137,14 @@ File.open(config_path) do |f|
   end
 end
 
+# First element is promised to be invalid, remove it:
+read_result.shift
+
+
 overrides = read_result.select { |rr| rr[0] == 'override'}
 layers = (read_result - overrides).map do |rr|
   Layer.new(rr[:header],
             rr[:contents].map { |c| Chord.new(c[0], c[1])})
 end
 
-ChordedKeeb.new(layers).as_string
+puts ChordedKeeb.new(layers).as_string
